7757d413f21dc580cd9ec6317acb120cff6b134d
Gen <gen@rose.dev>
2023-02-23 21:38:56 -0800

    ;)

--- quickocr/main.py
+++ quickocr/main.py
@@ -1,4 +1,5 @@
 import os
+import re
 from subprocess import call
 from PIL import Image, ImageEnhance, ImageFilter
 from mss import mss
@@ -8,49 +9,63 @@ import string
 import time
 import cv2
 import pytesseract
-from secure_delete import secure_delete
 import pyperclip
 from pynput.keyboard import Key, Listener
+import subprocess
+from urllib.parse import quote
 
-ShouldRun = True
+StopProgram = False
+ShouldRun = False
 
-secure_delete.secure_random_seed_init()
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
-screen_shot = mss()
-
 # crop screenshot of screen so it is only the top left
-def return_screenshot(mon={"top": 100, "left": 50, "width": 1820, "height": 750}):
-    screen_shot.get_pixels(mon)
-    img = Image.frombytes("RGB", (screen_shot.width, screen_shot.height), screen_shot.image)
-    img = np.array(img)
-    img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-    return img
+#def return_screenshot(mon={"top": 100, "left": 50, "width": 1820, "height": 750}):
+def return_screenshot():
+    with mss() as sct:
+        # The screen part to capture
+        monitor = {"top": 290, "left": 40, "width": 380, "height": 170}
+        # Grab the data
+        sct_img = sct.grab(monitor)
+        img = Image.frombytes("RGB", (sct_img.width, sct_img.height), sct_img.rgb)
+        img = np.array(img)
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        return img
 
 
 def on_press(key):
     #print('{0} pressed'.format(key))
-    if key == Key.shift_r:
-        return False
+    # this stops the processing of everything and prevents the key from being typed
+    if key == Key.f12:
+        ShouldRun = False
+        # doesn't pass press to on_release
+        #return False
 
 def on_release(key):
-    global ShouldRun
+    global ShouldRun, StopProgram
+    ShouldRun = False
     #print('{0} release'.format(key))
-    if key == Key.esc:
-        ShouldRun = False
+    # handle right shift taking a sc and text searching
+    if key == Key.shift_r:
+        ShouldRun = True
+        return False
+
+    # ... then since we would reset the variable here anyway on release
+    if key == Key.f12:
+        StopProgram = True
         return False
 
 
 print("Press RIGHT shift to capture | ESC to exit")
-while(ShouldRun):
+while(StopProgram == False):
     print("Ready")
     with Listener(on_press=on_press,
     on_release=on_release) as listener:
         listener.join()
-
-    #Would be set false if we just set it 
-    if ShouldRun is False:
+    if StopProgram == True:
         break
+    if ShouldRun == False: 
+        continue
 
     cv2.imwrite("cap.png", return_screenshot())
 
@@ -60,24 +75,24 @@ while(ShouldRun):
 
     #imageToProcess = imageToProcess.filter(ImageFilter.MedianFilter())
 
-    #enhancer = ImageEnhance.Contrast#(imageToProcess)
-    #imageToProcess = enhancer.enhance(5)
+    enhancer = ImageEnhance.Contrast(imageToProcess)
+    imageToProcess = enhancer.enhance(2)
+    
+    colorChangerEnhancer = ImageEnhance.Color(imageToProcess)
+    imageToProcess = colorChangerEnhancer.enhance(2)
     #
-    #colorChangerEnhancer = ImageEnhance.Color#(imageToProcess)
-    #imageToProcess = colorChangerEnhancer.#enhance(0.2)
-    #
-    #brightness = ImageEnhance.Brightness#(imageToProcess)
-    #imageToProcess = brightness.enhance(0.9)
+    #brightness = ImageEnhance.Brightness(imageToProcess)
+    #imageToProcess = brightness.enhance(0.5)
     #
     #sharpness = ImageEnhance.Sharpness#(imageToProcess)
     #imageToProcess = sharpness.enhance(20)
     #
     ##imageToProcess = imageToProcess.convert#('1')
-    #imageToProcess.save('cap.png')
+    imageToProcess.save('cap.png')
     #
 
     textToType = pytesseract.image_to_string(imageToProcess, lang='eng', config='--psm 1 --oem 3') #needed space at end
-
+    #textToType = pytesseract.image_to_string(imageToProcess, lang='eng')
     #with open('read_this.txt', 'w') as filehandle:
     #    filehandle.write(textToType)
     #
@@ -86,19 +101,31 @@ while(ShouldRun):
     #    type_out = file.read()
 
     #type_out = clean_typos(type_out)
-    type_out = textToType
+    type_out = textToType.strip()
 
     #print("\n" + type_out)
-
-    pyperclip.copy(type_out)
-
+    print(type_out)
+    # use regular expression to extract the middle portion of the text
+    the_question_text = re.search(r'\n\n(.(?:.|\n)+\?)', type_out)
+    if the_question_text:
+        the_question_text = the_question_text.group(1)
+    else:
+        the_question_text = type_out
+    # check if the middle portion ends with a question mark
+    ends_with_question = the_question_text.strip().endswith('?')
+    # fix line breaks, if line broken in image means they used a space and it went to next line
+    the_question_text = the_question_text.replace('\n', ' ').replace('\r', ' ')
+    # remove last character from the_question_text if it ends with a question mark
+    print(the_question_text)
+    if ends_with_question == True:
+        print("WTF??")
+        the_question_text = the_question_text[:-1]
+        print(the_question_text)
+    print(f"{the_question_text}\nEnds with a question mark: {ends_with_question}")
     # type the text
     #pyautogui.typewrite(type_out, interval=SPEED)
-
-    #keep a log NOT SECURE
-    #with open("log.txt", "a") as file:
-    #    file.write("\n" + type_out)
-
-
-    secure_delete.secure_delete('cap.png')
-    secure_delete.secure_delete('log.txt')
+    pyperclip.copy(the_question_text)
+    url = "https://www.google.com/search?q=" + the_question_text
+    #url = "https://www.google.com/search?q=" + quote(the_question_text)
+    print(url)
+    subprocess.Popen(['explorer', url])
